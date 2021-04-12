504c75e1eed8bf6621a6cf043a8ce7b97291ea7f
aragon12 <sm95007@gmail.com>
2021-04-12 09:29:41 +0530

    home: fix formatting
    add placholder php code

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
+<?php
+//placeholder, just to recognize as php
+?>
 <html>
     <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <style type="text/css">
         body{
             /*background-color:#26A69A;*/
@@ -36,20 +39,17 @@
     <body>
         <H1 align="center"> Select your quiz type</H1>
         <div class="btn_con">
-        <form method="Post" action="./quiz.php">
-            <button style="padding: 10px 64px 10px 64px;margin: 0px 0px 20px 0px" class="btn" name="qtype" value="easy">Easy</button><br>
-            
-            <button style="padding: 10px 29px 10px 29px ;margin: 0px 0px 20px 0px" class="btn" name="qtype" value="medium">Medium</button><br>
-            
-            <button style="padding: 10px 63px 10px 63px" class="btn" name="qtype" value="hard">Hard</button>
-            
-        </form>
+            <form method="Post" action="./quiz.php">
+                <button style="padding: 10px 64px 10px 64px;margin: 0px 0px 20px 0px" class="btn" name="qtype" value="easy">Easy</button><br />
+                <button style="padding: 10px 29px 10px 29px;margin: 0px 0px 20px 0px" class="btn" name="qtype" value="medium">Medium</button><br />
+                <button style="padding: 10px 63px 10px 63px" class="btn" name="qtype" value="hard">Hard</button>
+            </form>
         </div>
         <div class="inst_cont">
-            <div style="font-size: 25px;font-weight:bold;">Instructions:</div><br>
+            <div style="font-size: 25px;font-weight:bold;">Instructions:</div><br />
             <div class="inst">
-                • You will get only 30 seconds to attempt all questions.<br>
-                • After 30 seconds, your response will be submitted automatically.<br>
+                • You will get only 30 seconds to attempt all questions.<br />
+                • After 30 seconds, your response will be submitted automatically.<br />
                 • Do not refresh the browser window. you will loose your quiz.
             </div>
         </div>
